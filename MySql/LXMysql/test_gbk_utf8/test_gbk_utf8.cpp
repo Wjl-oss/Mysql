dbf393a9561d8d01d0292e2639c4dfbfdac68630
wangjialin <wangjialin@163.com>
2023-10-17 22:29:54 +0800

    hhhh 	deleted:    MySql/LXMysql/.vs/LXMysql/FileContentIndex/14c474da-a1d4-48c7-ba6e-f86199c33e5a.vsidx 	new file:   MySql/LXMysql/.vs/LXMysql/FileContentIndex/511246a9-b4e2-4d1c-bcce-e20d1c0068df.vsidx 	new file:   MySql/LXMysql/.vs/LXMysql/FileContentIndex/55b1c007-0482-4df6-828c-8e4a9dbe6c42.vsidx 	deleted:    MySql/LXMysql/.vs/LXMysql/FileContentIndex/821f424f-81c9-4836-aff7-ceca1905735b.vsidx 	new file:   MySql/LXMysql/.vs/LXMysql/FileContentIndex/af9d03e3-4fcd-4a1f-8139-cd6e73214996.vsidx 	deleted:    MySql/LXMysql/.vs/LXMysql/FileContentIndex/b3230a63-6103-47eb-bfb8-b9c227cece9f.vsidx 	modified:   MySql/LXMysql/.vs/LXMysql/v17/.suo 	modified:   MySql/LXMysql/.vs/LXMysql/v17/Browse.VC.db 	modified:   MySql/LXMysql/.vs/LXMysql/v17/Solution.VC.db 	new file:   MySql/LXMysql/.vs/LXMysql/v17/ipch/AutoPCH/b0f33ed12222b6da/VCTMP17728_397871.TEST_GBK_UTF8.00000000.ipch 	modified:   MySql/LXMysql/test_gbk_utf8/test_gbk_utf8.cpp 	modified:   MySql/LXMysql/test_gbk_utf8/x64/Debug/test_gbk_utf8.ilk 	modified:   MySql/LXMysql/test_gbk_utf8/x64/Debug/test_gbk_utf8.obj 	modified:   MySql/LXMysql/test_gbk_utf8/x64/Debug/vc143.idb 	modified:   MySql/LXMysql/test_gbk_utf8/x64/Debug/vc143.pdb 	modified:   MySql/LXMysql/x64/Debug/test_gbk_utf8.exe 	modified:   MySql/LXMysql/x64/Debug/test_gbk_utf8.pdb

--- MySql/LXMysql/test_gbk_utf8/test_gbk_utf8.cpp
+++ MySql/LXMysql/test_gbk_utf8/test_gbk_utf8.cpp
@@ -3,7 +3,29 @@
 
 #include <iostream>
 #include<string>
+#include<string.h>
+#ifdef _WIN32
 #include<Windows.h>
+#else
+#include<iconv.h>
+#endif
+
+#ifndef _WIN32
+static size_t Convert(char* from_cha, char* to_cha, char* in, size_t inlen, char* out, size_t outlen) {
+    iconv_t cd;
+    cd = iconv_open(to_cha, from_cha);
+    if (cd == 0) return -1;
+
+    memset(out, 0, sizeof out);
+    //返回转换字节数的数量
+    size_t re = iconv(cd, &in, &inlen, &out, &outlen);
+
+    if (cd != 0) iconv_close(cd);
+
+    return re;
+}
+#endif
+
 using std::string;
 using std::cout;
 using std::endl;
@@ -15,15 +37,14 @@ using std::wstring;
 
 //UTF8转GBK一般做法是UTF8先转unicode再转GBK，在WindowsVS中unicode编码一般是utf16
 string UTF8ToGBK(const char* data) {
-    string utf8;
-
-    //utf8转为unicode 在windows上unicode字符集使用utf16编码 不用平台编码方式可能不一样
-    
+    string re;
+#ifdef _WIN32
+    //utf8转为unicode 在windows上unicode字符集使用utf16编码 不用平台编码方式可能不一样    
     //将字符串映射到 UTF-16 (宽字符) 字符串。 字符串不一定来自多字节字符集。
     //https://learn.microsoft.com/zh-cn/windows/win32/api/stringapiset/nf-stringapiset-multibytetowidechar
-    int len=MultiByteToWideChar(CP_UTF8,0,data,-1,0,0);//计算出utf16占用多少字节
+    int len = MultiByteToWideChar(CP_UTF8, 0, data, -1, 0, 0);//计算出utf16占用多少字节
     if (len <= 0) {
-        return utf8;
+        return re;
     }
     wstring udata;
     udata.resize(len);
@@ -34,43 +55,59 @@ string UTF8ToGBK(const char* data) {
 
     //unicode转GBK
 
-    len=WideCharToMultiByte(CP_ACP, 0, (wchar_t*)udata.data(), -1, 0, 0, 0, 0);//获取宽字节转为多字节需要的空间大小
+    len = WideCharToMultiByte(CP_ACP, 0, (wchar_t*)udata.data(), -1, 0, 0, 0, 0);//获取宽字节转为多字节需要的空间大小
     if (len <= 0) {
-        return utf8;
+        return re;
     }
-    utf8.resize(len);
-    WideCharToMultiByte(CP_ACP, 0, (wchar_t*)udata.data(), -1, (char*)utf8.data(), len, 0, 0);
+    re.resize(len);
+    WideCharToMultiByte(CP_ACP, 0, (wchar_t*)udata.data(), -1, (char*)re.data(), len, 0, 0);
 
-    return utf8;
+#else
+
+    re.resize(1024);
+    int inlen = strlen(data);
+    Convert((char*)"utf-8", (char*)"gbk", (char*)data, inlen, (char*)re.c_str(), re.size());
+    int outlen = strlen(re.data());
+    re.resize(outlen);
+#endif
+
+    return re;
 }
 
-
-
 string GBKToUTF8(const char* data) {
-    string gbk;
-
+    string re;
+#ifdef _WIN32
     int len = MultiByteToWideChar(CP_ACP, 0, data, -1, 0, 0);//计算出utf16占用多少字节
     if (len <= 0) {
-        return gbk;
+        return re;
     }
     wstring udata;
     udata.resize(len);
-  
+
     MultiByteToWideChar(CP_ACP, 0, data, -1, (wchar_t*)udata.data(), len);
 
     //unicode转GBK
 
     len = WideCharToMultiByte(CP_UTF8, 0, (wchar_t*)udata.data(), -1, 0, 0, 0, 0);
     if (len <= 0) {
-        return gbk;
+        return re;
     }
-    gbk.resize(len);
-    WideCharToMultiByte(CP_UTF8, 0, (wchar_t*)udata.data(), -1, (char*)gbk.data(), len, 0, 0);
+    re.resize(len);
+    WideCharToMultiByte(CP_UTF8, 0, (wchar_t*)udata.data(), -1, (char*)re.data(), len, 0, 0);
 
-    return gbk;
+#else
+    re.resize(1024);
+    int inlen = strlen(data);
+    Convert((char*)"gbk", (char*)"utf-8", (char*)data, inlen, (char*)re.c_str(), re.size());
+    int outlen = strlen(re.data());
+    re.resize(outlen);
+#endif
+    return re;
 }
+
 int main()
 {
+#ifdef _WIN32
     std::cout << "Hello World! 王家林\n";
     //测试UTF8到GBK的转换
     cout << UTF8ToGBK(u8"测试UTF-8转GBK") << endl;//windows环境下的vs默认采用unicode字符集的utf16编码，也就是宽字符编码
@@ -82,17 +119,13 @@ int main()
     cout << "utf8 = " << utf8 << endl;//utf8在采用GBK编码的控制显示应该是乱码
     cout << UTF8ToGBK(utf8.c_str()) << endl;//转为GBK
 
-    
-
+#else
+    //linux环境下的默认字符集编码一般是采用utf8,转换为utf8之后输出是会乱码的
+    string in = u8"测试UTF-8转GBK";
+    cout << "输入 utf8 = " << in << endl;
+    string gbk = UTF8ToGBK(in.c_str());
+    cout << "gbk = " << gbk << endl;//linux默认是utf8，打印gbk会乱码
+    cout << "gbk转utf8 = " << GBKToUTF8(gbk.c_str()) << endl;
+#endif
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
